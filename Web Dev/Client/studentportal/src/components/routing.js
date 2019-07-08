4391c5891def2a7ffe66bcc3ccf216a70da92a33
shelby mirziteh <shelby.mirziteh@gmail.com>
2019-07-08 12:43:20 -0700

    linked dummy login page

--- Web Dev/Client/studentportal/src/components/routing.js
+++ Web Dev/Client/studentportal/src/components/routing.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Login from "./Login";
 
 const Routing = () => (
   <Switch>
-    {/* <Route exact path="/" component={homepage} />
-    <Route path="/signup" component={signup} /> */}
+    {/* <Route exact path="/" component={homepage} /> */}
+    <Route path="/login" component={Login} />
   </Switch>
 );
 
