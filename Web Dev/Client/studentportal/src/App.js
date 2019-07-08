c2b78c902275d1b587175210447350d8eedd07b5
shelby mirziteh <shelby.mirziteh@gmail.com>
2019-07-08 12:50:54 -0700

    added signup form and linked to homepage

--- Web Dev/Client/studentportal/src/App.js
+++ Web Dev/Client/studentportal/src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import { Link } from "react-router-dom";
 import Routing from "./components/routing";
 import Navigation from "./components/Navbar";
-import Login from "./components/Login";
 
 function App() {
   return (
     <div>
       <Navigation />
       <Link to="/login">Login</Link>
+      <Link to="/signup">signup</Link>
       <Routing />
     </div>
   );
--- Web Dev/Client/studentportal/src/components/routing.js
+++ Web Dev/Client/studentportal/src/components/routing.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./Login";
+import FormApp from "./SUform";
 
 const Routing = () => (
   <Switch>
     {/* <Route exact path="/" component={homepage} /> */}
     <Route path="/login" component={Login} />
+    <Route path="/signup" component={FormApp} />
   </Switch>
 );
 
