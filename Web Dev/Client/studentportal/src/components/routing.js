557d601ace25afe7de06cd5032d33f3dbbe1efb5
shelby mirziteh <shelby.mirziteh@gmail.com>
2019-07-08 10:27:32 -0700

    creating routing component, setup in app & idx

---
+++ Web Dev/Client/studentportal/src/components/routing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+const Routing = () => (
+  <Switch>
+    {/* <Route exact path="/" component={homepage} />
+    <Route path="/signup" component={signup} /> */}
+  </Switch>
+);
+
+export default Routing;
